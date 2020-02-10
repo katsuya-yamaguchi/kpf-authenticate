9df0f91da4b2f142050de990678cadf571597676
Katsuya003 <bb.win.3@gmail.com>
2020-02-11 01:20:23 +0900

    サインインコンポーネントをReduxを使用するように修正

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { useDispatch  } from 'react-redux';
+import { login } from '../actions/auth';
 
 const SignIn: React.FC = props => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const user = {
+      email: e.target.email.value,
+      password: e.target.password.value
+    }
+    dispatch(login(user))
+  }
+
   return (
     <div>
       <h1>SignIn</h1>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type='text'
+          name='email'
+          placeholder='Email'
+        />
+        <input 
+          type='password'
+          name='password'
+          placeholder='Password'
+        />
+        <button type='submit'>SingIn</button>
+      </form>
     </div>
   )
 }
