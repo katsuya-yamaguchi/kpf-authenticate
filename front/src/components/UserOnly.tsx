f1439a41eb854b552f3c77bdd85ec40b471f30de
Katsuya003 <bb.win.3@gmail.com>
2020-03-25 16:11:52 +0900

    lint

--- front/src/components/UserOnly.tsx
+++ front/src/components/UserOnly.tsx
@@ -3,7 +3,6 @@ import { useDispatch } from 'react-redux'
 import Button from '@material-ui/core/Button'
 import { fetchLoginStatus } from '../actions/auth'
 
-
 const UserOnly: React.FC = props => {
   const dispatch = useDispatch()
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -13,11 +12,7 @@ const UserOnly: React.FC = props => {
   return (
     <div>
       <h1>UserOnly</h1>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={handleClick}
-      >
+      <Button variant="contained" color="primary" onClick={handleClick}>
         LogOut
       </Button>
     </div>
