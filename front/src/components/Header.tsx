6557f671dc7ebc023557526703c61e5e287d8279
Katsuya003 <bb.win.3@gmail.com>
2020-03-25 17:32:45 +0900

    Menuの表示を更新。

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,24 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Menu,
+  MenuItem,
+  Typography
+} from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    button: {
+      color: 'rgba(255,255,255,1)'
+    },
+    title: {
+      flexGrow: 1
+    }
+  })
+)
 
 const Header: React.FC = props => {
+  const [anchorEl, setAnchorEl] = useState()
+  const classes = useStyles()
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(e.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
   return (
     <div>
-      <h1>Header</h1>
-      <ul>
-        <li>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            className={classes.button}
+            edge="start"
+            onClick={handleClick}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">KPF AUTH</Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+        <MenuItem onClick={handleClose}>
           <Link to="/">Home</Link>
-        </li>
-        <li>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
           <Link to="/useronly">UserOnly</Link>
-        </li>
-        <li>
-          <Link to="/signin">SingIn</Link>
-        </li>
-        <li>
-          <Link to="/signUp">SingUp</Link>
-        </li>
-      </ul>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link to="/signin">SignIn</Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link to="/signup">SignUp</Link>
+        </MenuItem>
+      </Menu>
     </div>
   )
 }
