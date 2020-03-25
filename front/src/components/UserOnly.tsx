d4ce3f5c7ec214c3005a1114b283cd49b32aaece
Katsuya003 <bb.win.3@gmail.com>
2020-03-25 16:11:08 +0900

    ログアウト処理を追加。

--- front/src/components/UserOnly.tsx
+++ front/src/components/UserOnly.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import Button from '@material-ui/core/Button'
+import { fetchLoginStatus } from '../actions/auth'
+
 
 const UserOnly: React.FC = props => {
+  const dispatch = useDispatch()
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    dispatch(fetchLoginStatus(false))
+  }
+
   return (
     <div>
       <h1>UserOnly</h1>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleClick}
+      >
+        LogOut
+      </Button>
     </div>
   )
 }
