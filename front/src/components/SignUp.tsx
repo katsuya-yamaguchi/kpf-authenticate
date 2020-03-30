cc83e6bce3b296798210e7859bd6c974054f6cd4
Katsuya003 <bb.win.3@gmail.com>
2020-03-30 14:40:29 +0900

    signinコンポーネントをとりあえずコピーしてsignupコンポーネントを作成。

--- front/src/components/SignUp.tsx
+++ front/src/components/SignUp.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { login } from '../actions/auth'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { thunkLogin } from '../actions/auth'
 import { userType } from '../constants/auth'
+import { RootState } from '../store'
+
+import { TextField, Button } from '@material-ui/core'
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
+import { useHistory } from 'react-router-dom'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      justifyContent: 'center'
+    },
+    button: {
+      margin: '10px auto',
+      display: 'block'
+    }
+  })
+)
 
 const SignUp: React.FC = props => {
   const dispatch = useDispatch()
+  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn)
+  const history = useHistory()
+
+  const classes = useStyles()
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -12,17 +34,40 @@ const SignUp: React.FC = props => {
       email: e.currentTarget.email.value,
       password: e.currentTarget.password.value
     }
-    dispatch(login(user))
+    dispatch(thunkLogin(user))
   }
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      history.push('/useronly')
+    }
+  })
 
   return (
     <div>
-      <h1>SignUp</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="email" placeholder="Email" />
-        <input type="password" name="password" placeholder="Password" />
-        <button type="submit">SingUp</button>
-      </form>
+      <div className={classes.root}>
+        <h1>SignUp</h1>
+      </div>
+      <div className={classes.root}>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <TextField id="standard-basic" name="email" label="Email" />
+          </div>
+          <div>
+            <TextField type="password" name="password" label="Password" />
+          </div>
+          <div>
+            <Button
+              className={classes.button}
+              variant="contained"
+              color="primary"
+              type="submit"
+            >
+              SignUp
+            </Button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
