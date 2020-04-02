4663a8de68f197007a5ccc1bc257f3cef1475470
Katsuya003 <bb.win.3@gmail.com>
2020-04-02 15:36:53 +0900

    fetchLoginStatusアクションのユニットテストコードを作成。

--- front/src/tests/actions/auth.spec.ts
+++ front/src/tests/actions/auth.spec.ts
@@ -1,5 +1,5 @@
-import { fetchLoadUserData } from '../../actions/auth'
-import { AuthActionType, FETCH_LOAD_USER_DATA } from '../../constants/auth'
+import { fetchLoadUserData, fetchLoginStatus, thunkLogin } from '../../actions/auth'
+import { AuthActionType, FETCH_LOAD_USER_DATA, FETCH_LOGIN_STATUS, userType, AuthStateType } from '../../constants/auth'
 
 describe('fetchLoadUserData', () => {
   it('is to suceed in fetching user data.', () => {
@@ -19,3 +19,24 @@ describe('fetchLoadUserData', () => {
     expect(fetchLoadUserData(state)).toEqual(expectedAction)
   })
 })
+
+describe('fetchLoginStatus', () => {
+  it('it is to suceed in fetching login status.', () => {
+    const state: Boolean = true
+    const expectedAction: AuthActionType = {
+      type: FETCH_LOGIN_STATUS,
+      isLoggedIn: true
+    }
+    expect(fetchLoginStatus(state)).toEqual(expectedAction)
+  })
+
+  it('it is to failed in fetching login status.', () => {
+    const state: Boolean = false
+    const expectedAction: AuthActionType = {
+      type: FETCH_LOGIN_STATUS,
+      isLoggedIn: false
+    }
+    expect(fetchLoginStatus(state)).toEqual(expectedAction)
+  })
+})
+
